8cdabd3bbe0fe7b1a6f5a23fc82933de437ff9c7
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-19 10:45:55 +0530

    for in method in list

--- P17.dart
+++ P17.dart
@@ -6,4 +6,9 @@ void main (){
   }
 }
 
-//if you dont know length of list then you will use   for(int i=0;i< number.length ;i++)
+//if you dont know length of list then you will use   for(int i=0;i< number.length ;i++) 
+
+//easy method in list  
+//for(int x in numbers){
+//print(x);
+//}
