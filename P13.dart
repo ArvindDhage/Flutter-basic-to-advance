8359813e0efbe9384a61b58303617ed50b4ba7d9
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-19 10:15:40 +0530

    Multiple condition in if else

---
+++ P13.dart
@@ -0,0 +1,15 @@
+//Multiple if or multiple condition 
+void main (){
+  int x=10;
+  int y=20;
+  if(x<5){
+    print('First condition');
+  }else if(y>10){
+    print("second condition ");
+
+  }else if (x==y){
+    print('Third Condition ');
+  }else {
+    print('Else');
+  }
+}
