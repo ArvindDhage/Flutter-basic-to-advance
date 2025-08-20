0c37fe27ab4e142d4ea0d041288fff6f5bd60b03
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-20 22:17:20 +0530

    Inheritance

---
+++ P23.dart
@@ -0,0 +1,13 @@
+//Inheritance 
+
+import 'P25.dart';
+
+class Student extends Person{
+  int? age;   //if you can use late keyword to dont creat private othervise use question mark " ?"
+  int? marks;
+
+}
+
+void main(){
+
+}
---
+++ P24.dart
@@ -0,0 +1,15 @@
+import 'P23.dart';
+import 'P25.dart';
+
+void main(){
+  Student student= Student();
+  student.marks=34;
+  student.roll =65;
+  student.age=56;
+  student.name='Arvind ';
+  print(student.name);
+  print(student.marks);
+  print(student.roll);
+  print(student.age);
+
+}
