0f4384e2fd22afe99a4e74785d7c40c74c02e230
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-20 23:20:26 +0530

    Finally Block

---
+++ P34.dart
@@ -0,0 +1,24 @@
+//Exception Handling 
+
+void main(){
+
+  try{
+    List<int> number =[3,5,2];
+  
+       print(number[3]);
+  }catch(e) {
+    print('Eception occur'); //Eception occur
+    //print(e); //RangeError (length): Invalid value: Not in inclusive range 0..2: 3
+
+  }catch (e){
+    print(e); //For other exception thes block are worked 
+  }finally{
+    print('Finally'); //work in any condition print end in the output 
+  }
+
+  
+      
+
+  
+      
+}
