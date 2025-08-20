2a34f5d6ddacb9c4108f8eda7624bc713538ee5d
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-20 23:16:44 +0530

    Exception Management

--- P32.dart
+++ P32.dart
@@ -7,8 +7,11 @@ void main(){
   
        print(number[3]);
   }catch(e) {
-    print('Eception occur');
-    print(e);
+    print('Eception occur'); //Eception occur
+    //print(e); //RangeError (length): Invalid value: Not in inclusive range 0..2: 3
+
+  }catch (e){
+    print(e); //For other exception thes block are worked 
   }
 
   
