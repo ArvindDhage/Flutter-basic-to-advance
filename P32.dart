554f42ec62053fbf093396bd21e1a3dbcf85e1fb
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-20 23:05:12 +0530

    Exception Handling

---
+++ P32.dart
@@ -0,0 +1,19 @@
+//Exception Handling 
+
+void main(){
+
+  try{
+    List<int> number =[3,5,2];
+  
+       print(number[3]);
+  }catch(e) {
+    print('Eception occur');
+    print(e);
+  }
+
+  
+      
+
+  
+      
+}
