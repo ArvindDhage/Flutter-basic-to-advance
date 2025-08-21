dc40db05c98da7ed6a95bb3521d15589132859e4
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-21 22:21:32 +0530

    Dart Asynchronous Programming – Then Function

--- P35.dart
+++ P35.dart
@@ -7,9 +7,9 @@ void main (){
 //   print("Fetching data ...");
 // await Future.delayed(Duration(seconds: 4));
 // print('Data fetched successfully');
-// } */
+// } */ // if you use await the we used async .
 
-void fetchData() async{
+void fetchData() {
   print("Fetching data ...");
  Future.delayed(Duration(seconds: 4)).then((value){
 print('Data fetched successfully');
@@ -17,4 +17,5 @@ print('Data fetched successfully');
 print('Other operation ');
 }
 
+//if used then function this no need to use await and asynic 
 
