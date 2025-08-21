9d0e431c6c3049ce4727f9890b60eecd7efffb83
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-21 23:25:14 +0530

    Implement keyword use as a extends

---
+++ P42.dart
@@ -0,0 +1,18 @@
+//Implement keyword use as a extends
+
+import 'P43.dart';
+
+class Student implements Person{
+  @override
+  int? age;
+
+  @override
+  String? name;
+
+  @override
+  void showData() {
+    // TODO: implement showData
+  }
+  
+
+}
