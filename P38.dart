0e3ff9aa6ecc2180333e06d15bbf110d72616a24
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-21 23:02:59 +0530

    //Collection keyword and Map

---
+++ P38.dart
@@ -0,0 +1,12 @@
+//Collection keyword and Map 
+void main(){
+  List<int> numbers =[1,2,3,5];
+  print(numbers[0]);
+
+  Map<String, dynamic>map={
+    'name':'Arvind',
+    'age':'21'
+
+  };
+  print(map['name']);
+}
