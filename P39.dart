ff829ba61c52c7832b2429cfe9318eb5d17ba24c
Arvind Dhage <arvinddhage35@gmail.com>
2025-08-21 23:08:52 +0530

    enum is used to stored the  constant values.

---
+++ P39.dart
@@ -0,0 +1,10 @@
+// enum is used to stored the  constant values.
+void main (){
+  print(Gender.Male.index);
+  print(Gender.Male.name);
+
+
+}
+enum Gender {Male, Female ,Other }
+
+enum Days {Sunday ,Monday ,Tuesday}
